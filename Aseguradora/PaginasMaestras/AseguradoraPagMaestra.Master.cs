61e4c168dfddfcd1936a2ff0d6af2ae22d9e964a
xGrillZ <stevenalonso99@hotmail.com>
2020-11-15 16:55:49 -0600

    Recreación página Maestra
    Se recreó la página maestra para evitar problemas con el nombre de la carpeta y archivo.

--- Aseguradora/PaginasMaestras/AseguradoraPagMaestra.Master.cs
+++ Aseguradora/PaginasMaestras/AseguradoraPagMaestra.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Aseguradora.MasterPage
+namespace Aseguradora.PaginasMaestras
 {
-    public partial class Aseguradora : System.Web.UI.MasterPage
+    public partial class AseguradoraPagMaestra : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
